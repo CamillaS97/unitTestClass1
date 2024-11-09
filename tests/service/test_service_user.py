eb3d4f76139c484b3e522c74b6f141c54e37beae
Camilla Silvestre <camillasilvestre@hotmail.com>
2024-11-09 16:31:28 -0300

    Adding test to get user job by user name

--- tests/service/test_service_user.py
+++ tests/service/test_service_user.py
@@ -36,3 +36,11 @@ class TestServiceUser:
         self.service_user.add_user("Camilla", "QA Engineer")
         result = self.service_user.remove_user("Camilla", "QA Engineer")
         assert result == expected_result
+
+    def test_get_user_job_success(self):
+        expected_result = "QA Engineer"
+        self.service_user = ServiceUser()
+        self.service_user.add_user("Camilla", expected_result)
+        result = self.service_user.get_user_by_name("Camilla")
+        assert result == expected_result
+        
