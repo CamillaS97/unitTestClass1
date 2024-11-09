2f3901c909e6d28c4ea87bd99c72325f6258d41b
Camilla Silvestre <camillasilvestre@hotmail.com>
2024-11-09 16:25:13 -0300

    Creating tests and methods

--- tests/service/test_service_user.py
+++ tests/service/test_service_user.py
@@ -5,18 +5,34 @@ from src.service.service_user import ServiceUser
 
 class TestServiceUser:
 
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.service_user = ServiceUser()
-
     def test_add_user_success(self):
-        result = self.service_user.add_user("Camilla", "Cargo Teste")
-        assert result == "User added"
+        self.service_user = ServiceUser()
+        expected_result = "User added"
+        result = self.service_user.add_user("Camilla", "QA Engineer")
+        assert result == expected_result
 
     def test_add_user_empty_string_fail(self):
-        result = self.service_user.add_user("", "Cargo Teste")
-        assert result == "Empty parameters not allowed"
+        self.service_user = ServiceUser()
+        expected_result = "Empty parameters not allowed"
+        result = self.service_user.add_user("", "QA Engineer")
+        assert result == expected_result
 
     def test_add_user_invalid_string_fail(self):
-        result = self.service_user.add_user(3, "Cargo Teste")
-        assert result == "Invalid parameters"
+        self.service_user = ServiceUser()
+        expected_result = "Invalid parameters"
+        result = self.service_user.add_user(3, "QA Engineer")
+        assert result == expected_result
+
+    def test_update_user(self):
+        expected_result = "Eduardo"
+        self.service_user = ServiceUser()
+        self.service_user.add_user("Camilla", "QA Engineer")
+        result = self.service_user.update_user("Camilla", expected_result)
+        assert result.name == expected_result
+
+    def test_remove_user(self):
+        expected_result = "User removed"
+        self.service_user = ServiceUser()
+        self.service_user.add_user("Camilla", "QA Engineer")
+        result = self.service_user.remove_user("Camilla", "QA Engineer")
+        assert result == expected_result
